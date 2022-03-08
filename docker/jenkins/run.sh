d2033efd39ca9eb29b20c557e63a188c6c660cac
morozovme <m.e.morozov1@gmail.com>
2022-03-08 17:44:00 -0500

    get passwd

--- docker/jenkins/run.sh
+++ docker/jenkins/run.sh
@@ -1,1 +1,5 @@
 sudo docker run -p 8080:8080 --name=jenkins-master -d --env JAVA_OPTS="-Xmx1024m" --env JENKINS_OPTS=" --handlerCountMax=300" jenkins/jenkins
+
+
+# get password
+# docker exec -it my-jenkins cat /var/jenkins_home/secrets/initialAdminPassword
