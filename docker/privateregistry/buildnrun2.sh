9bfe1cc9e76980541e4aa24d138fe64c99855abd
root <root@k8s-master.home>
2022-05-16 10:12:47 +0000

    remove dashes from names

--- docker/privateregistry/buildnrun2.sh
+++ docker/privateregistry/buildnrun2.sh
@@ -2,13 +2,13 @@ docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 docker pull ubuntu:16.04
 
-docker tag ubuntu:16.04 k8s-master:5000/my-ubuntu
+docker tag ubuntu:16.04 k8smaster.home:5000/myubuntu
 
-docker push k8s-master:5000/my-ubuntu
+docker push k8smaster.home:5000/myubuntu
 
 docker image remove ubuntu:16.04
 
-docker image remove k8s-master:5000/my-ubuntu
+docker image remove k8smaster.home:5000/myubuntu
 
 
-docker pull k8s-master:5000/my-ubuntu
+docker pull k8smaster.home:5000/myubuntu
